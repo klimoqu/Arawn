3ec8d8b3d38522ef6c2356264a289e5398fbe9d3
klimoqu <morta@digititus.itk.ppke.hu>
2011-12-28 12:57:33 +0100

    Update CORE/command.hpp

--- CORE/command.hpp
+++ CORE/command.hpp
@@ -5,15 +5,36 @@ class Command
 {
     int id,msgtype,msg;
 public:
-    Command(int _id, int _msgtype,int _msg)
+    Command(int _id, int _msgtype, int _msg)
     {
         id=_id;
         msgtype=_msgtype;
         msg=_msg;
     }
+    Command(){}
     int GetPlayerId(){return id;}
     int GetMessageType(){return msgtype;}
     int GetMessage(){return msg;}
+
+    friend QDataStream & operator<<(QDataStream & stream, Command & what);
+    friend QDataStream & operator>>(QDataStream & stream, Command & what);
 };
+
+QDataStream & operator >> ( QDataStream & stream, Command & what )
+{
+    stream  >> what.id;
+    stream  >> what.msgtype;
+    stream  >> what.msg;
+    return( stream );
+}
+
+QDataStream & operator << ( QDataStream & stream, Command & what )
+{
+    stream  << what.id;
+    stream  << what.msgtype;
+    stream  << what.msg;
+    return( stream );
+}
+
 
 #endif // COMMAND_HPP
